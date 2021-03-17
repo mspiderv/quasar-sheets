92b9d4f4aaaf5eb182e8b65c1070402d985ac89b
mspiderv <mspiderv@gmail.com>
2021-03-17 21:35:29 +0100

    sheets

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,55 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding class="q-gutter-x-md">
+
+    <h5>Sheets</h5>
+
+    <sheet title="Sheet A" class="q-pa-md" ref="sheetA">
+      <p>This is some sheet A content ...</p>
+      <div class="q-gutter-x-md">
+        <q-btn label="Show Sheet B" color="green" @click="$refs.sheetB.show()" />
+        <q-btn label="Show Sheet C" color="red" @click="$refs.sheetC.show()" />
+      </div>
+    </sheet>
+    <q-btn label="Show Sheet A" color="blue" @click="$refs.sheetA.show()" />
+
+    <sheet title="Sheet B" class="q-pa-md" ref="sheetB">
+      <p>This is some sheet B content ...</p>
+      <div class="q-gutter-x-md">
+        <q-btn label="Show Sheet A" color="blue" @click="$refs.sheetA.show()" />
+        <q-btn label="Show Sheet C" color="red" @click="$refs.sheetC.show()" />
+      </div>
+    </sheet>
+    <q-btn label="Show Sheet B" color="green" @click="$refs.sheetB.show()" />
+
+    <sheet title="Sheet C" class="q-pa-md" ref="sheetC">
+      <p>This is some sheet C content ...</p>
+      <div class="q-gutter-x-md">
+        <q-btn label="Show Sheet A" color="blue" @click="$refs.sheetA.show()" />
+        <q-btn label="Show Sheet B" color="green" @click="$refs.sheetB.show()" />
+      </div>
+    </sheet>
+    <q-btn label="Show Sheet C" color="red" @click="$refs.sheetC.show()" />
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import Sheet from 'components/Sheet'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    Sheet,
+  },
+  setup () {
+    const sheet2 = ref(false)
+    const sheet3 = ref(false)
+
+    return {
+      sheet2,
+      sheet3,
+    }
+  }
 })
 </script>
